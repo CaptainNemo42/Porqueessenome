df7a01d9a9caab397ade924797b0b6ac47cc3359
João V. <joao.nemo42@gmail.com>
2024-07-10 17:19:22 -0300

    Adição de questões

--- pqessenome/lib/Screens/Quizz/screens/DAENT/models/questionstopDAENT.dart
+++ pqessenome/lib/Screens/Quizz/screens/DAENT/models/questionstopDAENT.dart
@@ -2,93 +2,107 @@ import 'package:pqessenome/Screens/Quizz/screens/DAENT/models/questionDAENT.dart
 
 const List<Question> questions = [
   Question(
-    question: '1. Qual o Topônimo?',
-    correctAnswerIndex: 1,
-    options: [
-      'a) Madrid',
-      'b) Paris',
-      'c) Berlin',
-      'd) Rome',
-    ],
-  ),
-  Question(
-    question: '2. Qual o Topônimo?',
-    correctAnswerIndex: 3,
-    options: [
-      'a) Europe',
-      'b) Asia',
-      'c) North America',
-      'd) South America',
-    ],
-  ),
-  Question(
-    question: '3. Qual o Topônimo?',
-    correctAnswerIndex: 1,
-    options: [
-      'a) Earth',
-      'b) Jupiter',
-      'c) Saturn',
-      'd) Venus',
-    ],
-  ),
-  Question(
-    question: '4. Qual o Topônimo?',
-    correctAnswerIndex: 0,
-    options: [
-      'a) Nile',
-      'b) Amazon',
-      'c) Mississippi',
-      'd) Danube',
-    ],
-  ),
-  Question(
-    question: '5. Qual o Topônimo?',
+    question: '1. Qual a definição correta para o topônimo "Dimensiotopônimo"?',
     correctAnswerIndex: 2,
     options: [
-      'a) Hermione Granger',
-      'b) Ron Weasley',
-      'c) Harry Potter',
-      'd) Neville Longbottom',
+      'a) Relativo aos nomes próprios',
+      'b) Relativo as dimensões espaciais',
+      'c) Relativo a acidentes geográficos',
+      'd) Relativo a dimensões paralelas',
     ],
   ),
   Question(
-    question: '6. Qual o Topônimo?',
-    correctAnswerIndex: 3,
-    options: [
-      'a) Venus',
-      'b) Mars',
-      'c) Earth',
-      'd) Mercury',
-    ],
-  ),
-  Question(
-    question: '7. Qual o Topônimo?',
+    question: '2. Quais são exemplos dos "Antrotopônimos"?',
     correctAnswerIndex: 0,
     options: [
-      'a) William Shakespeare',
-      'b) Oscar Wilde',
-      'c) Jane Austen',
-      'd) Charles Dickens',
+      'a) Batista campos, Campina',
+      'b) Reduto, Umarizal',
+      'c) Marco, Vila',
+      'd) Reduto, Campina',
     ],
   ),
   Question(
-    question: '8. Qual o Topônimo?',
+    question:
+        '3. Ao analisar os topônimos a seguir, qual a informação correta?',
     correctAnswerIndex: 1,
     options: [
-      'a) Mont Blanc',
-      'b) Everest',
-      'c) Kilimanjaro',
-      'd) Aconcagua',
+      'a) Animotopônimo, relativo a fauna',
+      'b) Cronotopônimo, relativo a cor',
+      'c) Litotopônimo, referente a litorais',
+      'd) Fitotopônimo, relativo a vegetação',
     ],
   ),
   Question(
-    question: '9. Qual o Topônimo?',
+    question:
+        '4. Com relação ao topônimo "Hagiotopônimo", qual a informação verdadeira?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Relativo aos Deuses Tupi',
+      'b) Relativo ao caráter religioso',
+      'c) Relativo aos santos em geral',
+      'd) Ilustrado no bairro Marco',
+    ],
+  ),
+  Question(
+    question: '5. Qual dos seguintes é exemplo do Topônimo "Fitotopônimo"?',
     correctAnswerIndex: 3,
     options: [
-      'a) Starry Night',
-      'b) The Persistence of Memory',
-      'c) The Last Supper',
-      'd) Mona Lisa',
+      'a) Reduto',
+      'b) Marco',
+      'c) Aeroporto',
+      'd) Campina',
+    ],
+  ),
+  Question(
+    question:
+        '6. Ao analisar os topônimos a seguir, qual a informação correta?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Numerotopônimo, relativo a ordenação',
+      'b) Morfotopônimo, referente a estruturas morfológicas',
+      'c) Historiotopônimo, relativo a figuras históricas',
+      'd) Meteorotônimo, referente a meteorologia',
+    ],
+  ),
+  Question(
+    question: '7. Qual a definição correta para o topônimo "Cronotopônimo"?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Relativo a coloração do local',
+      'b) Referente ao caráter nostálgico',
+      'c) Relativo ao caráter cronológico',
+      'd) Referente a natureza temporária',
+    ],
+  ),
+  Question(
+    question: '8. Qual o Topônimo do bairro Nazaré?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Antrotopônimo',
+      'b) Historiotopônimo',
+      'c) Hagiotopônimo',
+      'd) N.D.A',
+    ],
+  ),
+  Question(
+    question: '9. Qual o Topônimo relacionado aos nomes próprios?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Antropotopônimo, de natureza antropocultural',
+      'b) Antrotopônimo, de natureza antropocultural',
+      'c) Sociotopônimo, de natureza física',
+      'd) Somatopônimo, de natureza física',
+    ],
+  ),
+  Question(
+    question:
+        '10. Ao analisar os topônimos a seguir, qual a informação incorreta?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Antrotopônimo, relativo aos nomes próprios',
+      'b) Hagiotopônimo, referente a culinária',
+      'c) Astrotopônimo, referente aos corpos celestes',
+      'd) Somatopônimo, relativo as partes do corpo',
     ],
   ),
 ];
